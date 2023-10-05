b1ee54894578a64fd522accf409f4bd79f8a5982
YoungMin Cho (Luke) <83196845+luke-youngmin-cho@users.noreply.github.com>
2023-10-05 17:56:43 +0900

    CSharp - Boxing, Vector, GC, Thread
    1. Boxing
    - Concept of object type.
    - Boxing & Unboxing.
    
    2. Vector
    - Concept of Vector.
    - Dot & Cross product.
    
    3. GC
    - Concept of Garbage collection.
    - Generation.
    
    4. Thread
    - Basic of thread.

---
+++ CSharpBasic/Boxing/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Boxing
+{
+    class Dummy { }
+    enum State { None, }
+    internal class Program
+    {
+        // 이 함수의 결과는 어떤 정수값을 넣던 false.
+        // 매개변수에 인자를 전달하는 순간 박싱이 일어나면서 새로운 객체가 만들어지기때문
+        private static bool CompareNum(object num)
+        {
+            object _num = 3;
+            return _num == num;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CompareNum(3));
+            int a = 3;
+            object obj1 = 1; // boxing
+            object obj2 = 1.4;
+            object obj3 = "Luke";
+            object obj4 = new Dummy();
+            object obj5 = State.None;
+            a = (int)obj1; // unboxing
+            double d1 = (double)obj2;
+            string name = (string)obj3;
+            Dummy dummy = (Dummy)obj4;
+            State state = (State)obj5;
+            object obj6 = 8;
+            float f = (float)obj2;
+        }
+
+    }
+}
