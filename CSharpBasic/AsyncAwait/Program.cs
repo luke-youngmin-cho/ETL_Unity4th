0007789dc9e114f19e0c5b78d7d4998335f1b062
YoungMin Cho (Luke) <83196845+luke-youngmin-cho@users.noreply.github.com>
2023-12-12 17:40:43 +0900

    CSharp - MultiThread
    - Concept of MultiThreading.
    - Threading with C# (Thread, ThreadPool, Task, async & await)

---
+++ CSharpBasic/AsyncAwait/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Net.Http;
+
+namespace AsyncAwait
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Task<bool> uselessTask = SayHi5Times();
+            uselessTask.Wait();
+            Console.WriteLine($"{uselessTask.Result}");
+
+            bool result = await SayHi5Times();
+            Console.WriteLine(result);
+
+            string url = "https://jsonplaceholder.typicode.com/todos/3";
+            string content = await RequestHttpContentAsync(url);
+            Console.WriteLine(content);
+        }
+
+        static async Task<string> RequestHttpContentAsync(string url)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+                string content = await response.Content.ReadAsStringAsync();
+                return content;
+            }
+        }
+
+
+        static async Task<bool> Dummy()
+        {
+            return await SayHi5Times();
+        }
+
+        static Task<bool> SayHi5Times()
+        {
+            return
+                Task.Run(() =>
+                {
+                    for (int i = 0; i < 5; i++)
+                    {
+                        Console.WriteLine($"Hi {i} times");
+                        Thread.Sleep(500);
+                    }
+                    return true;
+                });
+        }
+    }
+}
---
+++ CSharpBasic/MultiThreading/Program.cs
@@ -0,0 +1,103 @@
+﻿namespace MultiThreading
+{
+    internal class Program
+    {
+        static void StartMakingToast()
+        {
+            int count = 0;
+            while (count < 5)
+            {
+                Console.WriteLine($"{Thread.CurrentThread.Name} : Making toast ...");
+                Thread.Sleep(1000);
+                count++;
+            }
+            Console.WriteLine($"{Thread.CurrentThread.Name} : Made toast !");
+        }
+
+        static void Counting()
+        {
+            for (int i = 0; i < 100000; i++)
+            {
+                count++;
+                Console.WriteLine(count++);
+                PPAF(ref count);
+            }
+        }
+
+        static int PPAF(ref int a)
+        {
+            int tmp = a;
+            a = tmp + 1;
+            return tmp;
+        }
+        static int PPBF(ref int a)
+        {
+            a = a + 1;
+            return a;
+        }
+
+        static void ThreadPoolCallback(object? threadContext)
+        {
+            Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId} Thread {(int)threadContext} started");
+        }
+
+
+        static int count;
+
+        static void Main(string[] args)
+        {
+            //ThreadPool.SetMinThreads(1, 0);
+            //ThreadPool.SetMaxThreads(4, 0);
+            //
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    ThreadPool.QueueUserWorkItem(ThreadPoolCallback, i);
+            //}
+/*
+
+            Thread thread1 = new Thread(Counting);
+            thread1.Name = "Thread 1";
+            thread1.IsBackground = true;
+            thread1.Start();
+
+            Thread thread2 = new Thread(Counting);
+            thread2.Name = "Thread 2";
+            thread2.IsBackground = true;
+            thread2.Start();
+
+            thread1.Join();
+            thread2.Join();*/
+
+            // Task : 쓰레드 풀의 쓰레드에 단일 작업을 할당하는 클래스
+            Task t1 = new Task(StartMakingToast);
+            t1.Start();
+            t1.Wait();
+
+            Task t2 = new Task(StartMakingToast);
+            t2.Start();
+            t2.Wait();
+
+            List<Task> tasks = new List<Task>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                tasks.Add(Task.Factory.StartNew(StartMakingToast));
+            }
+
+            Task.WaitAll(tasks.ToArray());
+
+            Task<int> taskWithResult = new Task<int>(() =>
+            {
+                return new Random().Next(10);
+            });
+            taskWithResult.Start();
+
+            taskWithResult.Wait();
+            Console.WriteLine($"Task with random result : {taskWithResult.Result}");
+
+
+            Console.WriteLine($"Count : {count}");
+            Console.ReadLine();
+        }
+    }
+}
